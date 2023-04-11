59376b65a8fddbf8a6bf275920cd53290739e096
vpetrovaa <vpetrova.laba@solvd.com>
2023-04-11 19:30:49 +0300

    Imports were fixed

--- src/main/java/com/solvd/rentals/web/controller/RentalController.java
+++ src/main/java/com/solvd/rentals/web/controller/RentalController.java
@@ -9,7 +9,12 @@ import com.solvd.rentals.web.mapper.AggregateMapper;
 import com.solvd.rentals.web.mapper.RentalMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import reactor.core.publisher.Mono;
 
 @RestController
