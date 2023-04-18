1c8eb55e60831dcdffd4aee59c8f7903acbcdee1
vpetrovaa <vpetrova.laba@solvd.com>
2023-04-18 16:14:56 +0300

    FindByCarNumber method was fixed

--- src/main/java/com/solvd/rentals/query/impl/QueryServiceImpl.java
+++ src/main/java/com/solvd/rentals/query/impl/QueryServiceImpl.java
@@ -3,7 +3,7 @@ package com.solvd.rentals.query.impl;
 import com.solvd.rentals.aggregate.RentalAggregate;
 import com.solvd.rentals.query.FindByCarNumberQuery;
 import com.solvd.rentals.query.QueryService;
-import com.solvd.rentals.repository.EventRepository;
+import com.solvd.rentals.repository.RentalRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -14,12 +14,12 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 public class QueryServiceImpl implements QueryService {
 
-    private final EventRepository eventRepository;
+    private final RentalRepository rentalRepository;
 
     @Override
     public Mono<RentalAggregate> handle(FindByCarNumberQuery query) {
         log.info("Find name of rental method was called");
-        return eventRepository.findByCarNumber(query.carNumber());
+        return rentalRepository.findByCarNumber(query.carNumber());
     }
 
 }
--- src/main/java/com/solvd/rentals/repository/EventRepository.java
+++ src/main/java/com/solvd/rentals/repository/EventRepository.java
@@ -1,12 +1,8 @@
 package com.solvd.rentals.repository;
 
-import com.solvd.rentals.aggregate.RentalAggregate;
 import com.solvd.rentals.event.Event;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import reactor.core.publisher.Mono;
 
 public interface EventRepository extends ReactiveMongoRepository<Event, String> {
-
-    Mono<RentalAggregate> findByCarNumber(String carNumber);
 
 }
